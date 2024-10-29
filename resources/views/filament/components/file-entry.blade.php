bafd1533b86d5563ca61cb694a2f8c91529ff3af
hoceineel <hoceineelidrissi@gmail.com>
2024-10-29 14:31:35 +0100

    fix potential error

--- resources/views/filament/components/file-entry.blade.php
+++ resources/views/filament/components/file-entry.blade.php
@@ -1,6 +1,6 @@
 <div class="flex items-center space-x-2">
     @if ($getState())
-        <a href="{{ $getState() }}" target="_blank"
+        <a href="{{ $getState() }}" target="_blank" rel="noopener noreferrer"
             class="inline-flex items-center px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition duration-150 ease-in-out">
             <x-heroicon-o-document-text class="w-5 h-5 mr-2" />
             <span>{{ __('filament-modular-subscriptions::fms.file_entry.view_file') }}</span>
